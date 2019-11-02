d96844d6df44a3c74c5c8080d91b115cc9602c38
Chris <saffronkrzysztof@gmail.com>
2019-11-02 01:07:30 +0100

    Full navbar ready, content style present, awaiting populating of actual content

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,7 @@
 // STICKY BAR DISPLAY
 
-  const navbar = document.querySelector('.stick-nav');
-  const hiddenDiv = document.getElementById('hidden-div');
+  const navbar = document.querySelector('.stick-nav'),
+        hiddenDiv = document.getElementById('hidden-div');
 
 window.onscroll = function() {
   checkStickyBar();
@@ -16,3 +16,37 @@ function checkStickyBar() {
   }
   console.log('ding');
 }
+
+// MOBILE MENU BAR START
+
+const menuBarMobile = document.getElementById('menu-bar-mobile'),
+      mobileMenu = document.getElementById('mobile-menu');
+
+const menuMobile = function() {
+  if (document.body.classList.contains('navMode')) {
+    document.body.classList.remove('navMode');
+    mobileMenu.classList.remove('mobile-menu-start');
+    mobileMenu.classList.add('mobile-menu-end');
+
+  } else {
+    document.body.classList.add('navMode');
+    mobileMenu.classList.remove('hidden');
+    mobileMenu.classList.remove('mobile-menu-end');
+    mobileMenu.classList.add('mobile-menu-start');
+  }
+}
+
+mobileMenu.addEventListener('animationend', function() {
+  if (!document.body.classList.contains("navMode")) {
+    mobileMenu.classList.add('hidden');
+  }
+})
+
+menuBarMobile.addEventListener('click', menuMobile);
+
+
+// MOBILE MENU CLOSE
+
+const closeMenuMobile = document.querySelector('.close-mobile-menu');
+
+closeMenuMobile.addEventListener('click', menuMobile);
